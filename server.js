942402484f20a83da4b54a71039b8ea9494b1870
SONofOdin1 <srivarschool@gmail.com>
2021-10-14 11:57:54 +0530

    first commit

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const express = require('express');
 const app = express();
 
 // app.get command function res.send 
+
 app.get('/', (req, res) => res.send('Working'));
 // Create Port with first const port and create process.env.PORT || and port
+
 const PORT = process.env.PORT || 3000;
-// 
+// Now app.listen command to PORT Function (confirm ${PORT})
+
+app.listen(PORT, () => console.log(`working on ${PORT} port`));
+
