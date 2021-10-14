d485e8065ab805137a013bc920c87e69c38a6d27
SONofOdin1 <srivarschool@gmail.com>
2021-10-14 14:51:09 +0530

    first commit

---
+++ config/db.js
@@ -0,0 +1,5 @@
+// create default.json import mongo URI 
+// bring in mongoose, config and db from mongoURI
+const mongoose = require('mongoose');
+const config = require('config');
+const db = require('mongoURI');
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 // First create express and app
 const express = require('express');
+const { connect } = require('mongoose');
 const app = express();
 
 // app.get command function require and respond and res.send 
 
-app.get('/', (req, res) => res.send('Working'));
+app.get('/', (req, res) => res.send('My Name is Srivar'));
 
 // Create Port with first const port and create process.env.PORT || and port
 
@@ -13,7 +14,6 @@ const PORT = process.env.PORT || 3000;
 // App.listen (port, use console.log for checking)
 
 app.listen(PORT, () => console.log(`working on ${PORT} port`));
-
 
 
 
