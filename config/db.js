b2f8d08c7c4270078e2756ed164a6068638a9927
SONofOdin1 <srivarschool@gmail.com>
2021-10-15 16:09:22 +0530

    first commit

--- config/db.js
+++ config/db.js
@@ -1,5 +1,24 @@
 // create default.json import mongo URI 
-// bring in mongoose, config and db from mongoURI
+// bring in mongoose, config 
+// const db with config.get('mongoURI')
 const mongoose = require('mongoose');
 const config = require('config');
-const db = require('mongoURI');
+const db = config.get('mongoURI');
+
+// const connectDB with async function {try{await connect to mongoose to db check with console.log }  catch (err) console.error(err.message) then exit process at 1}; 
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db);
+
+        console.log('Connected to DB');
+    } catch (err) {
+        console.error(err.message);
+        
+        process.exit(1);
+    }
+};
+
+// then module.exports the connectDB command
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-// First create express and app
+// First create express and app and const connectDB to the config folder like ./config/db
 const express = require('express');
-const { connect } = require('mongoose');
+const connectDB = require('./config/db');
+
 const app = express();
+
+// Use command connectDB like connectDB();
+
+connectDB();
 
 // app.get command function require and respond and res.send 
 
-app.get('/', (req, res) => res.send('My Name is Srivar'));
+app.get('/', (req, res) => res.send('My Server is not working'));
 
 // Create Port with first const port and create process.env.PORT || and port
 
